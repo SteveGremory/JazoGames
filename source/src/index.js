51e3d71586254b5a81ab83ef540f5d65fff5f584
Steve Gremory <38187217+SteveGremory@users.noreply.github.com>
2020-10-17 21:21:10 +0530

    lol

--- source/src/index.js
+++ source/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Countdown from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Countdown
+      timeTillDate="12 01 2021, 6:00 am"
+      timeFormat="MM DD YYYY, h:mm a"
+    />
   </React.StrictMode>,
   document.getElementById("root")
 );
